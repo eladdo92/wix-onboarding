1b71c0664d9bdcb2a2c2af1d2de36eba0ad73cc5
Vlad <vladk@wix.com>
2021-03-08 10:35:33 +0200

    works statically

---
+++ phase1/dropdown.js
@@ -0,0 +1,12 @@
+function run() {
+    document.getElementById("testcontainer").innerHTML = 
+    "<select name=\"Animals\" id=\"animals\" class=\"dropdown\"> \
+        <option selected class=\"stamp\">Favorite animal...</option> \
+        <option>Monkey</option> \
+        <option>Lemur</option> \
+        <option>Tune</option> \
+        <option>Chipmunk</option> \
+        <option>Gazelle</option> \
+        <option>Human</option> \
+    </select>";
+}
