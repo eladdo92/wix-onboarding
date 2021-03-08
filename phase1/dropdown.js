72f0e22c46a0a03f948cc9c1729c57866b08fab0
Vlad <vladk@wix.com>
2021-03-08 11:48:51 +0200

    feat(phase1): solution with selector

--- phase1/dropdown.js
+++ phase1/dropdown.js
@@ -1,12 +1,32 @@
-function run() {
-    document.getElementById("testcontainer").innerHTML = 
-    "<select name=\"Animals\" id=\"animals\" class=\"dropdown\"> \
-        <option selected class=\"stamp\">Favorite animal...</option> \
-        <option>Monkey</option> \
-        <option>Lemur</option> \
-        <option>Tune</option> \
-        <option>Chipmunk</option> \
-        <option>Gazelle</option> \
-        <option>Human</option> \
-    </select>";
+function addSelector(root) {
+    const dropDown = makeDropDown('Animals', 'animals', ['Monkey', 'Lemur', 'Tuna', 'Chipmunk', 'Gazelle', 'Human']);
+    root.appendChild(dropDown);
+}
+
+function makeDropDown(name, id, options) {
+    const selectNode = document.createElement('select');
+    selectNode.setAttribute('name', name);
+    selectNode.setAttribute('id', id);
+    selectNode.setAttribute('class', 'dropdown');
+    const stampNode = makeStamp('Favorite animal...');
+    selectNode.appendChild(stampNode);
+    for (const option of options) {
+        const optionTag = makeOption(option);
+        selectNode.appendChild(optionTag);
+    }
+    return selectNode;
+}
+
+function makeStamp(text) {
+    const stampNode = makeOption(text);
+    stampNode.setAttribute('selected', 'true');
+    stampNode.setAttribute('class', 'stamp');
+    return stampNode;
+}
+
+function makeOption(text) {
+    const optionNode = document.createElement('option');
+    const textNode = document.createTextNode(text);
+    optionNode.appendChild(textNode);
+    return optionNode;
 }
